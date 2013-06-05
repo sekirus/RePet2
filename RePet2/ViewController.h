db5b665d7b65526b8e7ea27780291c38e223a727
sergey <sergey@sergeys-iMac.local>
2013-06-05 17:43:27 +0400

    Initial Commit

---
+++ RePet2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RePet2
+//
+//  Created by sergey on 6/5/13.
+//  Copyright (c) 2013 SeKiRus Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RePet2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RePet2
+//
+//  Created by sergey on 6/5/13.
+//  Copyright (c) 2013 SeKiRus Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
